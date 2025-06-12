311649f5ef0d3f2df4faae0e5320d91e40d41965
Gabriel Keven Domingues de Souza <gabriel-keven@outlook.com>
2025-06-12 15:14:47 -0300

    Inserção dos ícones do projeto

--- projeto/settings.py
+++ projeto/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Nós temos apps também
+    'recipes',
 ]
 
 MIDDLEWARE = [
